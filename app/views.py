71ffd508dd36eb5e2970185b57209b52cc7c18fe
Alireza Berenjian <Ali.rberenjian@gmail.com>
2019-07-09 16:46:56 -0700

    Add File into Content

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpRequest
 from django.shortcuts import render
+import requests
 
 pages = [
     {
@@ -24,36 +25,40 @@ pages = [
 ]
 
 def index(request):
-    file_content = open('./app/contents/index.html').read()
+    # file_content = open('./app/index.html').read()
     content = {
-        'content': file_content,
+        # 'content': file_content,
         'pages': pages,
         'title': 'Home'
     }
-    return render(request,'app/base.html',content)
+    return render(request,'app/index.html',content)
 def about(request):
-    file_content = open('./app/contents/about.html').read()
+    # file_content = open('./app/contents/about.html').read()
     content = {
-        'content': file_content,
+        # 'content': file_content,
         'pages': pages,
         'title': 'About'
     }
-    return render(request,'app/base.html',content)
+    return render(request,'app/about.html',content)
 
 def projects(request):
-    file_content = open('./app/contents/projects.html').read()
+    response = requests.get('https://api.github.com/users/aberenjian89/repos')
+    repos = response.json
+    print(repos)
+    # file_content = open('./app/contents/projects.html').read()
     content = {
-        'content': file_content,
+        # 'content': file_content,
         'pages': pages,
+        'repos': repos,
         'title': 'Projects'
     }
-    return render(request,'app/base.html',content)
+    return render(request,'app/projects.html',content)
 
 def contact(request):
-    file_content = open('./app/contents/contact.html').read()
+    # file_content = open('./app/contents/contact.html').read()
     content = {
-        'content': file_content,
+        # 'content': file_content,
         'pages': pages,
         'title': 'Contact'
     }
-    return render(request,'app/base.html',content)
+    return render(request,'app/contact.html',content)
